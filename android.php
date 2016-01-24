752ebf28f76beb7f17c6ba7cf047851acfb93a64
diamen <mstopa23@gmail.com>
2016-01-24 13:58:27 +0100

    [JSON] Na razie brak walidacji użytkownika, jedynie odpowiedź o poprawnym zalogowaniu

--- android.php
+++ android.php
@@ -2,4 +2,12 @@
 
 $jsonInput = file_get_contents("php://input");
 $response = json_decode($jsonInput, true);
-print $response['hej'];
+
+$data = array(
+    'errno' => '0',
+    'error' => '',
+);
+
+$send = json_encode($data);
+
+echo $send;
