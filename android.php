113947dca95585d42af28d0aea00875940c1a329
achmen <achmenek@gmail.com>
2016-01-29 02:52:37 +0100

    Jsony

--- android.php
+++ android.php
@@ -1,5 +1,42 @@
 <?php
+#models
+require_once("models/User.php");
+require_once("models/Spot.php");
 
-$jsonInput = file_get_contents("php://input");
-$response = json_decode($jsonInput, true);
-print $response['hej'];
+class Android {
+
+    static function process(){
+
+        $json = json_decode(file_get_contents("php://input"));
+        $function = $json->function;
+
+        switch($function){
+            case "login":
+                $arr = self::login($json->username, $json->password);
+                self::login();
+                break;
+
+            case "getSpots":
+                break;
+
+        }
+
+        $response = json_encode($arr);
+        echo $response;
+    }
+
+    static function login($username,$password) {
+
+        $login = User::_login($username,$password);
+
+        if($login == true) {
+            $arr = array('succes'=>'true');
+        } else {
+            $arr = array('succes'=>'false');
+        }
+
+        return $arr;
+    }
+}
+
+Android::process();
--- models/User.php
+++ models/User.php
@@ -2,9 +2,7 @@
 
 require_once("Connections.php");
 require_once("smarty.php");
-
 class User {
-
     static function process_login() {
         if (user::checkLogin()) {
             header("Location: /console");
@@ -13,7 +11,6 @@ class User {
         $smarty = SSmarty::returnSmarty();
         if (isset($_POST["login"]) && isset($_POST["user"]) && isset($_POST["pass"])) {
             try {
-
                 if (self::login()) {
                     header("Location: /console");
                     die();
@@ -23,11 +20,8 @@ class User {
             }
             $smarty->assign("error", "Użytkownik, lub Hasło jest nieoprawne");
         }
-
-
         $smarty->display("login.tpl");
     }
-
     static function process_register() {
         
         if (user::checkLogin()) {
@@ -38,7 +32,6 @@ class User {
         $smarty = SSmarty::returnSmarty();
         if (isset($_POST["register"]) && isset($_POST["user"]) && isset($_POST["pass"]) && isset($_POST["email"])) {
             try {
-
                 if (self::createUser()&&self::login()) {
                     
                     header("Location: /console");
@@ -51,15 +44,11 @@ class User {
                 $smarty->assign("error", "Błąd bazy danych");
             }
         }
-
-
         $smarty->display("register.tpl");
     }
-
     static function checkLogin() {
         return isset($_SESSION["uid"]);
     }
-
     static function login() {
         $stmt = (new Database())->getConnection()->prepare("SELECT id FROM users WHERE login=? && pass=?");
         $stmt->execute(array($_POST["user"], $_POST["pass"]));
@@ -69,16 +58,21 @@ class User {
         }
         return false;
     }
-
+    static function _login($username,$pass) {
+        $stmt = (new Database())->getConnection()->prepare("SELECT id FROM users WHERE login=? && pass=?");
+        $stmt->execute(array($username, $pass));
+        if (($temp = $stmt->fetch()) != false) {
+            return true;
+        }
+        return false;
+    }
     static function getUID() {
         return (isset($_SESSION["uid"]) ? $_SESSION["uid"] : NULL);
     }
-
     static function logout() {
         
         unset($_SESSION["uid"]);
     }
-
     static function createUser() {
         $conn = (new Database())->getConnection();
         $stmt = $conn->prepare("INSERT INTO users (login,pass,email) VALUES (?,?,?)");
@@ -87,11 +81,9 @@ class User {
         }
         return false;
     }
-
     static function deleteUser() {
         $conn = (new Database())->getConnection();
         $stmt = $conn->prepare("DELETE FROM users WHERE id=?");
         $stmt->execute(array(self::getUID()));
     }
-
 }
